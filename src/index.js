12377eede1ea0a12dabeb1123206a6bd2aba6fe5
Sohaib Manah <souhaibemanah@gmail.com>
2023-03-05 22:03:51 +0100

    moving toaster to the top

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import Modal from "react-modal";
+import { Toaster } from "react-hot-toast";
 Modal.setAppElement("#root");
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App/>);
+root.render(
+  <>
+    <Toaster />
+    <App />
+  </>
+);
