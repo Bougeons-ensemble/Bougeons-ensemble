cae6199599a062d2ea2fcd5eb17d5945244aa7b2
ELYAHYAOUY-FL <110742439+ELYAHYAOUY-FL@users.noreply.github.com>
2023-03-12 23:58:42 +0100

    fix the regester erreur

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,31 +1,33 @@
+export function register() {
+    document.addEventListener('install', (event) => {
+        const urlsToCache = [
+          '/',
+          '/index.html',
+    
+        ];
+      
+        event.waitUntil(
+          caches.open('bougeons-ensemble-cache')
+            .then((cache) => {
+              return cache.addAll(urlsToCache);
+            })
+        );
+      });
+      
+      
+     document.addEventListener('fetch', (event) => {
+        event.respondWith(
+          caches.match(event.request)
+            .then((response) => {
+              if (response) {
+                return response;
+              }
+      
+              return fetch(event.request);
+            })
+        );
+      });
+    
+  }
 
 
-
-document.addEventListener('install', (event) => {
-    const urlsToCache = [
-      '/',
-      '/index.html',
-
-    ];
-  
-    event.waitUntil(
-      caches.open('bougeons-ensemble-cache')
-        .then((cache) => {
-          return cache.addAll(urlsToCache);
-        })
-    );
-  });
-  
-  
- document.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request)
-        .then((response) => {
-          if (response) {
-            return response;
-          }
-  
-          return fetch(event.request);
-        })
-    );
-  });
